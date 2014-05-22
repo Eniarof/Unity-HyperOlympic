ae9583dab1d7d5f051144f72584ef1dd27c94c0f
abstractmachine <douglas@abstractmachine.net>
2014-05-23 00:15:07 +0200

    Running demo

---
+++ HyperOlympic/Assets/Scripts/Run.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Run : MonoBehaviour
+{
+
+		public string whichKey;
+		
+		Animator animator;
+		GameObject ground;
+	
+		void Start ()
+		{
+	
+				animator = GetComponent<Animator> ();
+				ground = transform.FindChild ("Ground").gameObject;
+	
+		}
+	
+		void Update ()
+		{
+	
+				float speed = animator.GetFloat ("speed");
+		
+				if (Input.GetKeyDown (whichKey)) {
+						speed += 1.0f;
+				}
+		
+				speed = speed * 0.975f;
+		
+				if (speed < 0.2f) {
+						speed = 0.0f;
+				}
+		
+				animator.SetFloat ("speed", speed);
+				
+				ground.transform.Translate (-speed * 0.05f, 0, 0);
+	
+		}
+
+}

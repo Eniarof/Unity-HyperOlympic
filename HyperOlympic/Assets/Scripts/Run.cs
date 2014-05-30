410839e34a28d78b938eacb26c3372ae18fc6a8a
abstractmachine <douglas@abstractmachine.net>
2014-05-30 09:09:07 +0200

    Updated assets. Updated code/project. Demo version 0.1

--- HyperOlympic/Assets/Scripts/Run.cs
+++ HyperOlympic/Assets/Scripts/Run.cs
@@ -3,39 +3,49 @@ using System.Collections;
 
 public class Run : MonoBehaviour
 {
-
-		public string whichKey;
-		
 		Animator animator;
 		GameObject ground;
+		TextMesh timer;
+	
+		float time = 0.0f;
+		bool isRunning = true;
+		public string whichKey = "space";
 	
 		void Start ()
 		{
-	
 				animator = GetComponent<Animator> ();
-				ground = transform.FindChild ("Ground").gameObject;
-	
+				timer = transform.FindChild ("Timer").GetComponent<TextMesh> ();
+				ground = transform.parent.FindChild ("Ground").gameObject;
+				
+				Screen.showCursor = false;
 		}
 	
 		void Update ()
 		{
+				if (isRunning) {
+						time += Time.deltaTime;
+				}
+				timer.text = time.ToString ("0.00");
 	
 				float speed = animator.GetFloat ("speed");
-		
-				if (Input.GetKeyDown (whichKey)) {
+				if (isRunning && Input.GetKeyDown (whichKey)) {
 						speed += 1.0f;
 				}
-		
 				speed = speed * 0.975f;
-		
 				if (speed < 0.2f) {
 						speed = 0.0f;
 				}
-		
 				animator.SetFloat ("speed", speed);
 				
 				ground.transform.Translate (-speed * 0.05f, 0, 0);
+		}
 	
+	
+		void OnTriggerEnter2D (Collider2D other)
+		{
+				if (other.gameObject.name == "EndZone") {
+						isRunning = false;
+				}
 		}
 
 }
